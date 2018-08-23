5feb6a9b4d10c2a1efb8c1aeef8fcd352cf58a1c
Maxence Bouret <maxence.bouret@gmail.com>
2018-08-23 10:38:28 +0200

    Iteration 1 to 6

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,9 +10,37 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(punkAPIBeers =>{
+    let data={
+      beersForView: punkAPIBeers
+    };
+    res.render('beers',data); // Renders "views/beers.hbs"
+  })
+  .catch(error => {
+    console.log('error')
+  })
+});
 
-app.listen(3000);
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random-beer',{
+      beer: beers[0]
+    }); // Renders "views/random-beer.hbs"
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000")
+});
